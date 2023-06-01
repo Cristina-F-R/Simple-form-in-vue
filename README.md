88e60bae26d45c0483a74d823f03bf39302b3b51
CrisFaRo <cristina.faixedas@gmail.com>
2023-06-01 10:53:24 +0200

    add simple form in vue 3

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple form in Vue
+
+A simple form to have a firt aprox to this framework. 
+
+## How to open the project: 
+
+This project is created with Vite. You can clone the repository into your pc, wirte into the terminal: *npm i* to install all the dependencies and then *npm run dev* to open the projent into the browser. 
+
+## Author: 
+
+Cristina Faixedas
---
+++ src/App.vue
@@ -0,0 +1,92 @@
+
+<template>
+<h1> Simple From</h1>
+<p>{{form}} </p>
+  <form>
+
+    <label>Email: </label>
+    <input type="text" placeholder=" enter email" v-model="form.email" />
+    <br/>
+    <br/>
+    <label>Password: </label>
+    <input type="password" placeholder=" enter password" v-model="form.password"/>
+    <br/>
+    <br/>
+    <select v-model='form.country'>
+      <option> Spain </option>
+      <option> France </option>
+      <option> Italy </option>
+    </select>
+    <br/>
+    <br/>
+    <h3> Gender </h3>
+    <label>Male: </label>
+    <input type="radio" name="gender" value="male" v-model="form.gender" />
+    <br/>
+    <label>Femle: </label>
+    <input type="radio" name="gender" value="female" v-model="form.gender" />
+    <br/>
+    <br/>
+    <h3> Technology </h3>
+    <label>React: </label>
+    <input type="checkbox" value="java" v-model="form.technology" />
+    <br/>
+    <label>.Net: </label>
+    <input type="checkbox" value=".net" v-model="form.technology" />
+    <br/>
+    <br/>
+
+
+    <button type="button" v-on:click="login"> Login </button>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data () {
+    return {
+      form: {
+        email: '', 
+        password: '',
+        country: '',
+        gender: '', 
+        technology:[]
+      },
+      error: [],
+    }
+  },
+  methods:{
+    login (){
+
+      console.warn(this.form)
+      for(const item in this.form){
+        if(this.form[item] === "" || this.form[item].length === 0){
+          this.error.push(item)
+        }
+        if(this.error.length === 0){
+          alert('Data han been submitted')
+        }
+        console.warn(error)
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
